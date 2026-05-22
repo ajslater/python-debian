2bdbfe00e601cff1e83c68019ee176062ac23118
AJ Slater <aj@slater.net>
2026-05-22 15:05:53 -0700

    update devenv

--- bin/docker/install-binfmt-platforms.sh
+++ bin/docker/install-binfmt-platforms.sh
@@ -10,14 +10,14 @@ echo "$DOCKER_PASS" | docker login --username="$DOCKER_USER" --password-stdin
 # install emulator binaries if i need to
 EMULATORS=
 if [[ ${PLATFORMS-} == "linux/armhf" ]]; then
-  # this is the only arch i need to cross compile on circleci
-  EMULATORS=arm
+	# this is the only arch i need to cross compile on circleci
+	EMULATORS=arm
 elif [[ "$(uname -m)" == "aarch64" ]]; then
-  EMULATORS=aarch64
+	EMULATORS=aarch64
 fi
 
 if [[ -n ${EMULATORS-} ]]; then
-  docker run --rm --privileged tonistiigi/binfmt:latest --install "$EMULATORS"
+	docker run --rm --privileged tonistiigi/binfmt:latest --install "$EMULATORS"
 fi
 # buildx requires creating a builder on a fresh system
 docker buildx create --use
--- usr/local/sbin/entrypoint.sh
+++ usr/local/sbin/entrypoint.sh
@@ -3,19 +3,19 @@
 set -eu
 
 if [ "${DOCKER_IMAGE_DEPRECATED:-}" != "" ]; then
-  echo "--------------------------------------------------------"
-  echo "⚠️  WARNING: This image is DEPRECATED ⚠️"
-  echo "$DOCKER_IMAGE_DEPRECATED"
-  echo "--------------------------------------------------------"
+	echo "--------------------------------------------------------"
+	echo "⚠️  WARNING: This image is DEPRECATED ⚠️"
+	echo "$DOCKER_IMAGE_DEPRECATED"
+	echo "--------------------------------------------------------"
 fi
 
 if [ "${PUID:-}${PGID:-}" != "" ]; then
-  /usr/local/sbin/moduser.sh
-  LOCAL_USER_SCRIPT=/usr/local/sbin/moduser-local.sh
-  if [ -f "$LOCAL_USER_SCRIPT" ]; then
-    "$LOCAL_USER_SCRIPT"
-  fi
-  exec su abc -c "$@"
+	/usr/local/sbin/moduser.sh
+	LOCAL_USER_SCRIPT=/usr/local/sbin/moduser-local.sh
+	if [ -f "$LOCAL_USER_SCRIPT" ]; then
+		"$LOCAL_USER_SCRIPT"
+	fi
+	exec su abc -c "$@"
 else
-  exec "$@"
+	exec "$@"
 fi
